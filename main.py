8bbf5313eeacac921d84ca5f94341ebda721d9d5
aman122002 <114601513+aman122002@users.noreply.github.com>
2026-01-19 18:55:32 +0000

    audio detection script

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Project starting")
+import sounddevice as sd
+import numpy as np
+
+def audio_callback(indata, frames, time, status):
+    volume = np.linalg.norm(indata)
+    print(volume)
+
+with sd.InputStream(channels=1, samplerate=16000, callback=audio_callback):
+    print("Listening...")
+    while True:
+        pass
